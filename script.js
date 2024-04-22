f17a1c1eec054ebb3ce4e8dcafae6e92c0a662a8
willi301 <willi301willi@gmail.com>
2024-04-21 23:52:50 -0700

    edit table javascript

--- script.js
+++ script.js
@@ -41,21 +41,33 @@ function submitFunction() {
     var tbodyRef = document.getElementById('myTable').getElementsByTagName('tbody')[0];
 
 
+    for (const [word, frequency] of Object.entries(top5)) {
+        // Insert a row at the end of table
+        var newRow = tbodyRef.insertRow();
+
+        // Insert a cell at the end of the row
+        var cell1 = newRow.insertCell(0);
+        var cell2 = newRow.insertCell(1);
+
+        // Append a text node to the cell
+        var newText = document.createTextNode(word);
+        var newNumber = document.createTextNode(frequency);
+        cell1.appendChild(newText);
+        cell2.appendChild(newNumber);
+       
+    }
+
+
+    /*
 
     for(const word of wordList) {
         if(top5[word]) {
-            // Insert a row at the end of table
-            var newRow = tbodyRef.insertRow();
-
-            // Insert a cell at the end of the row
-            var newCell = newRow.insertCell();
-
-            // Append a text node to the cell
-            var newText = document.createTextNode(word);
-            newCell.appendChild(newText);
+            
         }
 
     }
+
+    */
 
 
 
